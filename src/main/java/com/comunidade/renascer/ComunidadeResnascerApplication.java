b967d358130243a91e78f69793b647af0169a0a0
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-12-02 17:13:10 -0400

    feat: project squad 12 with complete dependencies

---
+++ src/main/java/com/comunidade/renascer/ComunidadeResnascerApplication.java
@@ -0,0 +1,13 @@
+package com.comunidade.renascer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunidadeResnascerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunidadeResnascerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comunidade/renascer/ComunidadeResnascerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comunidade.renascer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunidadeResnascerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
